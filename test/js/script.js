925c35a5fdb1910a3b74eacf825fc356b0f59caa
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-03-05 11:37:58 +0200

    header

---
+++ test/js/script.js
@@ -0,0 +1,10 @@
+// jQuery(document).ready(function($){
+//   $('.header-arrow_1').hover(
+//       function () {
+//       $('header-dropdown', this).stop().slideDown(400);
+//       },
+//       function () {
+//         $('ul', this).stop().slideUp(400);
+//       }
+//     );
+//   });
