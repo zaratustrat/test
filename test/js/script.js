46b38f1c5bfe3a20470e4348741311c582b74174
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-03-07 17:07:51 +0200

    main

--- test/js/script.js
+++ test/js/script.js
@@ -1,32 +1,8 @@
-$('.main-wrap__card').slick({
- infinite: true,
- slidesToShow: 1,
- slidesToScroll: 1,
- responsive: [
-  {
-   breakpoint: 1024,
-   settings: {
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    infinite: true,
-    dots: true
-   }
-  },
-  {
-   breakpoint: 600,
-   settings: {
-    slidesToShow: 2,
-    slidesToScroll: 2
-   }
-  },
-  {
-   breakpoint: 480,
-   settings: {
-    slidesToShow: 1,
-    slidesToScroll: 1
-   }
-  }
-  
- ]
-});
+$('.slider-common').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  infinite: true
 
+
+});
