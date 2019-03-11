855259a0c79bca13516aaedddb5fceddf6e59edc
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-03-11 15:34:43 +0200

    advantages

--- test/js/script.js
+++ test/js/script.js
@@ -1,8 +1,64 @@
 $('.slider-common').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
-  arrows: false,
-  infinite: true
+  arrows: true,
+  infinite: true,
+  nextArrow: '<div class="arrow arrow_right"></div>',
+  prevArrow: '<div class="arrow arrow_left"></div>',
+  responsive: [{
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
+// этот скрипт срабатывает при разрешении выше 992ръ
+/* $(document).ready(function () {
+  $('.menu1').hover(
+    function () {
+      $(this).find('.header-dropdown').toggleClass('visible');
+    }
+  );
+  $('.menu2').hover(
+    function () {
+      $(this).find('.header-dropdown2').toggleClass('visible');
+    }
+  );
+});
+ */
 
 
+
+$(window).resize(function () {
+  if ($(window).width() < 992) {
+    $('.menu1').click(
+      function () {
+        $(this).find('.header-dropdown').toggleClass('visible');
+      }
+    );
+    $('.menu2').click(
+      function () {
+        $(this).find('.header-dropdown2').toggleClass('visible');
+      }
+    );
+  }
 });
